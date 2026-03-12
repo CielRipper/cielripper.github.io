7fe88f7c9a0af45eb37e600e32613a1ecab6dd6f
CielRipper <jaxontreadwell@gmail.com>
2026-03-12 16:28:20 -0500

    Testing Run and adding of movies.cs for tutorial part

--- csci340lab8/RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
+++ csci340lab8/RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7486f972064fabafeffb4faa6b43478947e5d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42e5d0a6d58c49ed6db08ef21c35759763623781")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

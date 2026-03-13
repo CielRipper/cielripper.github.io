ba8d3102ccf97186f7845e8a72eea862f1cb9ba3
CielRipper <jaxontreadwell@gmail.com>
2026-03-13 12:27:17 -0500

    Added models: Student, Enrollment, Course and made minor edits

--- csci340lab9/ContosoUniversity/Pages/Error.cshtml.cs
+++ csci340lab9/ContosoUniversity/Pages/Error.cshtml.cs
@@ -8,7 +8,6 @@ namespace ContosoUniversity.Pages;
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
-    public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
